fbd8ea195d020b1970d9411ec12e1631e60fbd16
Roperi <rodo@automatones.com>
2024-01-12 16:10:09 +0000

    Try Metadata to display OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -7,19 +8,30 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Invoicer',
   description: 'Clever invoicing',
+  image: 'https://mlmx.s3.us-west-1.amazonaws.com/invoicer/invoicer.png',
 }
 
 export default function RootLayout({
   children,
+  pageMetadata, // Additional metadata for the specific page
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
+  pageMetadata?: Metadata; // Additional metadata for the specific page
 }) {
+  const mergedMetadata = { ...metadata, ...pageMetadata };
+
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-vBfCLuCiDXF4QpYY6VzCEnt/jMNkspeN5x2cw46UCiBwETrwJSvFP9Tg86vGCr+Q6JNthdR8y5KnB/2J3C2Jpw==" crossOrigin="anonymous" />
-        <link property="og:image" content="https://mlmx.s3.us-west-1.amazonaws.com/invoicer/invoicer.png" />
-      </head>
+
+        <meta property="og:title" content={mergedMetadata.title} />
+        <meta property="og:description" content={mergedMetadata.description} />
+        <meta property="og:image" content={mergedMetadata.image} />
+
+        {/* Add more Open Graph tags if needed */}
+      </Head>
+
       <body className={inter.className}>
         {children}
       </body>
